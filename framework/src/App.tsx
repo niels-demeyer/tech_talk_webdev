d4b08f4fa6d84218925e03f08292a8f844c3584e
nelzon <niels.demeyer99@gmail.com>
2024-07-18 15:49:17 +0200

    changes

--- framework/src/App.tsx
+++ framework/src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -7,18 +7,30 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
-function App() {
+export default function App() {
   const images = ["pizza.jpg"]; // replace with your actual image names
 
   return (
-    <Carousel>
+    <Carousel
+      opts={{
+        align: "start",
+      }}
+      className="w-full max-w-sm"
+    >
       <CarouselContent>
         {images.map((image, index) => (
-          <CarouselItem key={index}>
-            <img
-              src={`${import.meta.env.BASE_URL}${image}`}
-              alt="carousel item"
-            />
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                <CardContent className="flex aspect-square items-center justify-center p-6">
+                  <img
+                    className="w-full h-auto object-cover"
+                    src={`${import.meta.env.BASE_URL}${image}`}
+                    alt="carousel item"
+                  />
+                </CardContent>
+              </Card>
+            </div>
           </CarouselItem>
         ))}
       </CarouselContent>
@@ -27,5 +39,3 @@ function App() {
     </Carousel>
   );
 }
-
-export default App;
